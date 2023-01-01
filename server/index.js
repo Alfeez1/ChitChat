6da01f59542796b6d7fe0d18991e22a1d3841792
Alfeez1 <Muhammed.alfeez@b-jelt.com>
2023-01-02 00:57:50 +0530

    fully setupped

--- server/index.js
+++ server/index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes.js');
-// const authRoutes = require('./routes/auth');
-// const messageRoutes = require('./routes/messages');
 const messageRoutes = require('./routes/messages.js');
 const app = express();
 const socket = require('socket.io');
 require('dotenv').config();
-
+const path = require('path');
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', userRoutes);
 app.use('/api/messages', messageRoutes);
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+// const MONGO_URL =
+//   'mongodb+srv://alfeez:alfeez@cluster0.2z0sxnr.mongodb.net/?retryWrites=true&w=majority';
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -25,9 +26,13 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+const PORT = 5000;
 const server = app.listen(process.env.PORT, () => {
   console.log(`server started on ${process.env.PORT}`);
 });
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+);
 const io = socket(server, {
   cors: {
     origin: 'http://localhost:3000',
